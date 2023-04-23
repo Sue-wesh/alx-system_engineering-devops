6b1d78d21fe7872e61085486c22f58bbd9dd6ccf
Susan <shi4git@gmail.com>
2023-04-23 12:40:07 -0700

    use puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,16 @@
 #set up client SSH configuration file to connect to a server without a passwd.
-class { 'ssh':
-  IdentityFile           => '~/.ssh/school'
-  PasswordAuthentication => 'no'
+include stdlib
+
+file_line { 'passwd auth':
+  ensure  => present,
+  path    => 'etc/ssh/ssh_config',
+  line    => '   PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'config private key':
+  ensure  => present,
+  path    => 'etc/ssh/ssh_config',
+  line    => '   IdentityFile ~/.ssh/school',
+  replace => true,
 }
