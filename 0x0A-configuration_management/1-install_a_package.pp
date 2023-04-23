cf5247efb89cebab3a552b9617c483fe62656e74
Susan <shi4git@gmail.com>
2023-04-23 13:02:20 -0700

    remove

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# using Puppet install flask from pip3, version 2.1.0
-#
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# using puppet create a manifest that kills a process killmenow
-
-exec { 'pkill -x killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
