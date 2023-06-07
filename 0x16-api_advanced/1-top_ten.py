44e9757a0069ef38027c50bf8fd4e0d1bd928fd7
Susan <shi4git@gmail.com>
2023-06-07 03:17:25 -0700

    top ten posts in reddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,6 +11,7 @@ def top_ten(subreddit):
     """
     print the titles of the first 10 hot posts listed for a subreddit
     """
+    subreddit = sys.argv[1]
     url = "http://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
     agent_string = "API-query-reddit"
 
