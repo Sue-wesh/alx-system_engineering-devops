99cf9d9df00921b9ecd5024aeef8cd10ccd92a28
Susan <shi4git@gmail.com>
2023-06-07 02:58:28 -0700

    number of reddit subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+return the total no of subscribers for a given subreddit
+"""
+import json
+import requests
+import sys
+
+def number_of_subscribers(subreddit):
+    """
+    if in invalid subreddit is given return 0
+    """
+    url = "http://api.reddit.com/r/{}/about".format(subreddit)
+    agent_string = "API-query-reddit"
+
+    subs = requests.get(url, headers={'User-Agent': agent_string})
+
+    if subs.status_code != 200:
+        return 0
+    else:
+        json_response = subs.json()
+        subs_num = json_response.get('data').get('subscribers')
+        return subs_num
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,7 @@
+0x16. API advanced
+
+Tasks
+0. How many subs?
+Write a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+
+
