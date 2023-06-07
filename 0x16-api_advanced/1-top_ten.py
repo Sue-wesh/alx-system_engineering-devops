b9425cb1e167bf16996eb8c8f02098cd11e3f684
Susan <shi4git@gmail.com>
2023-06-07 03:15:10 -0700

    top ten posts in reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+query redditApi
+"""
+import json
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """
+    print the titles of the first 10 hot posts listed for a subreddit
+    """
+    url = "http://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
+    agent_string = "API-query-reddit"
+
+    titles = requests.get(url, headers={'User-Agent': agent_string})
+
+    if titles.status_code != 200:
+        print(None)
+    else:
+        json_response = titles.json()
+        ten_titles = json_response.get('data').get('children')
+        for new in ten_titles:
+            title = new.get('data').get('title')
+            print(ten_titles)
