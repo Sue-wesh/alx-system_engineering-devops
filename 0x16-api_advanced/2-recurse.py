66f69dd31dafd56387cf2e9b2e68fd8423d9c2a4
Susan <shi4git@gmail.com>
2023-06-07 03:39:19 -0700

    number of all hot titles

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+queru Reddit API
+"""
+import json
+import requests
+import sys
+
+def recurse(subreddit, hot_list=[]):
+    """
+    return a list containing the titles of all hot articles for given subreddit
+    """
+    url = "http://api.reddit.com/r/{}/hot".format(subreddit)
+    u_agent = "API-query-reddit"
+
+    titles = requests.get(url, headers={'User-Agent': u_agent})
+
+    if titles.status_code != 200:
+        return None
+    else:
+        json_resp = titles.json()
+        hot_list.append(json_resp.get('data').get('title'))
+        return hot_list
